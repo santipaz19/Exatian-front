7881a0cb5db0640510fe15e2a96af7bfea4b54ea
Santiago Paz <santipaz19.02@gmail.com>
2025-06-13 19:20:24 -0300

    fix to deploy

--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const LOCAL_API_URL = "http://localhost:5000/";
 const REMOTE_API_URL = "https://exatian-back.onrender.com";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || LOCAL_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL || REMOTE_API_URL;
 
 const apiClient = axios.create({
     baseURL,
